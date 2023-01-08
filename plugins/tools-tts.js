cf9413ae70f512b630bc29ad8aabb358bc189e05
Manson Oso <92900375+MansonOsito@users.noreply.github.com>
2023-01-08 00:08:14 -0500

    Update tools-tts.js

--- plugins/tools-tts.js
+++ plugins/tools-tts.js
@@ -18,7 +18,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   catch (e) {
     m.reply(e + '')
     text = args.join(' ')
-    if (!text) throw `🧑🏻‍💻 Ejemplo : \n${usedPrefix}${command} hola como estas 👉🏻 hi how are you`
+    if (!text) throw `🧑🏻‍💻 Ejemplo : \n${usedPrefix}${command} Fuck you 👉🏻 Púdrete`
     res = await tts(text, defaultLang)
   } finally {
     if (res) conn.sendFile(m.chat, res, 'tts.opus', null, m, true)
