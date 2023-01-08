1369846cca1797bb0c81caf65fe34d04704a3190
Manson Oso <92900375+MansonOsito@users.noreply.github.com>
2023-01-07 23:52:03 -0500

    Update sticker-attp.js

--- plugins/sticker-attp.js
+++ plugins/sticker-attp.js
@@ -1,7 +1,7 @@
 
 import { sticker } from '../lib/sticker.js'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-	if (!text) throw `🧑🏻‍💻️ Agrega el texto\n\n👉🏻 Ejemplo  ${usedPrefix + command} Turbo Nitro`
+	if (!text) throw `🧑🏻‍💻️ Agrega el texto, pues PELOTUDO de daMier\n\n👉🏻 Ejemplo  ${usedPrefix + command} OsoBot`
     //conn.sendFile(m.chat, global.API('xteam', '/attp', { file: '', text }), 'attp.webp', '', m, false, { asSticker: true })
      let stiker = await sticker(null, global.API('xteam', '/attp', { file: '', text }), global.packname, global.author)
     if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m )
